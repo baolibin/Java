3ac428d8b40c33561206b4dd9418d7f6cf4206c9
baolibin <2578858653@qq.com>
2019-02-17 23:27:27 +0800

    update

---
+++ src/main/java/com/libin/data/StringDemo.java
@@ -0,0 +1,28 @@
+package com.libin.data;
+
+import java.util.ArrayList;
+
+/**
+ * Copyright (c) 2019/02/17. xixi Inc. All Rights Reserved.
+ * Authors: libin <2578858653@qq.com>
+ * <p>
+ * Purpose :
+ */
+public class StringDemo {
+    public static void main(String[] args) {
+        // format
+        System.out.println(String.format("this is %s", "demo"));
+        // join
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("村");
+        list.add("头");
+        list.add("干");
+        list.add("活");
+        list.add("那");
+        list.add("小");
+        list.add("伙");
+        System.out.println(String.join(",", list));
+
+
+    }
+}
