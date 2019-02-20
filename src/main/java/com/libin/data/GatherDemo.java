06a0a04fc6445402ea7fea09fe1363958fd7c6d8
baolibin <2578858653@qq.com>
2019-02-20 22:33:11 +0800

    update

---
+++ src/main/java/com/libin/data/GatherDemo.java
@@ -0,0 +1,10 @@
+package com.libin.data;
+
+/**
+ * Copyright (c) 2019/02/20. xixi Inc. All Rights Reserved.
+ * Authors: libin <2578858653@qq.com>
+ * <p>
+ * Purpose :
+ */
+public class GatherDemo {
+}
