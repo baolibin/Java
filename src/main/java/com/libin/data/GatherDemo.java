746f0dcf9561e090e9fc8c988591d3d39a97904b
baolibin <2578858653@qq.com>
2019-02-23 20:37:50 +0800

    update

--- src/main/java/com/libin/data/GatherDemo.java
+++ src/main/java/com/libin/data/GatherDemo.java
@@ -4,7 +4,10 @@ package com.libin.data;
  * Copyright (c) 2019/02/20. xixi Inc. All Rights Reserved.
  * Authors: libin <2578858653@qq.com>
  * <p>
- * Purpose :
+ * Purpose : 集合类Demo
  */
 public class GatherDemo {
+    public static void main(String[] args){
+
+    }
 }
