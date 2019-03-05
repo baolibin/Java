86139ad9490df97062e5ac777ae6367e19369965
baolibin <2578858653@qq.com>
2019-03-05 21:27:26 +0800

    update

--- src/main/java/com/libin/data/GatherDemo.java
+++ src/main/java/com/libin/data/GatherDemo.java
@@ -1,5 +1,8 @@
 package com.libin.data;
 
+import java.util.Hashtable;
+import java.util.Map;
+
 /**
  * Copyright (c) 2019/02/20. xixi Inc. All Rights Reserved.
  * Authors: libin <2578858653@qq.com>
@@ -7,7 +10,14 @@ package com.libin.data;
  * Purpose : 集合类Demo
  */
 public class GatherDemo {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        // Hashtable 无法存入null值
+        Hashtable<String, String> hashtable = new Hashtable<>();
+        hashtable.put("", "");
+        hashtable.put("null", "null");
+        for (Map.Entry entry : hashtable.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
     }
 }
